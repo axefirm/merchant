d6d37e785f5b96a2c4c35c9d8c0a2590aa097469
bilguungt <bilguungt2@gmail.com>
2021-01-11 10:00:05 +0800

    notifcation

---
+++ iBank/src/app/main/pages/notification/notif/notif.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+export interface Notif {
+  amount: string;
+  bankName: string;
+  correspondentName: string;
+  accountNo: string;
+  description: string;
+  date: string;
+  time: string;
+}
+
+
+export const NOTIFS: Notif[] = [
+  { bankName: 'XacBank', amount: '6000', correspondentName: "Enkhbayar", accountNo: '5000611662', date: "10 Feb 2020", description: "Vodka", time:"12:04" },
+  { bankName: 'XacBank', amount: '6000', correspondentName: "Enkhbayar", accountNo: '5000611662', date: "10 Feb 2020", description: "Vodka", time:"12:04" },
+  { bankName: 'XacBank', amount: '6000', correspondentName: "Enkhbayar", accountNo: '5000611662', date: "10 Feb 2020", description: "Vodka", time:"12:04" },
+  { bankName: 'XacBank', amount: '6000', correspondentName: "Enkhbayar", accountNo: '5000611662', date: "10 Feb 2020", description: "Vodka", time:"12:04" },
+  { bankName: 'XacBank', amount: '6000', correspondentName: "Enkhbayar", accountNo: '5000611662', date: "10 Feb 2020", description: "Vodka", time:"12:04" },
+  { bankName: 'XacBank', amount: '6000', correspondentName: "Enkhbayar", accountNo: '5000611662', date: "10 Feb 2020", description: "Vodka", time:"12:04" },
+];
+
+
+@Component({
+  selector: 'app-notif',
+  templateUrl: './notif.component.html',
+  styleUrls: ['./notif.component.scss']
+})
+export class NotifComponent implements OnInit {
+  notifs = NOTIFS;
+  selectedHero: Notif;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- iBank/src/app/main/pages/notification/notification.component.ts
+++ iBank/src/app/main/pages/notification/notification.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+
+
+
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
-export class NotificationComponent implements OnInit {
 
-  constructor() { }
+export class NotificationComponent implements OnInit {
+   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
