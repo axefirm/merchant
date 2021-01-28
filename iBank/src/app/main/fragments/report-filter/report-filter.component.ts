f094b0410743322ddda30bc6baa9e87f16833ffd
ganbat24 <s.ganbat24@gmail.com>
2021-01-28 21:22:55 +0800

    Report bk

--- iBank/src/app/main/fragments/report-filter/report-filter.component.ts
+++ iBank/src/app/main/fragments/report-filter/report-filter.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   FormControl,
   Validators,
 } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-report-filter',
@@ -13,14 +14,35 @@ import {
 })
 export class ReportFilterComponent implements OnInit {
   hide = true;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private dialogRef: MatDialogRef<ReportFilterComponent>,
+    @Inject(MAT_DIALOG_DATA) data
+  ) {}
 
   main: FormGroup;
+  types: string[] = ['type1', 'type2'];
+  channels: string[] = ['type1', 'type2'];
+  currencies: string[] = ['type1', 'type2'];
+  senderNumbers: string[] = ['type1', 'type2'];
+  recieverNumbers: string[] = ['type1', 'type2'];
+  transactionNumbers: string[] = ['type1', 'type2'];
+
+  cancel() {
+    this.dialogRef.close();
+  }
+  save() {
+    this.dialogRef.close(this.main.value);
+  }
 
   ngOnInit(): void {
     this.main = this.formBuilder.group({
-      dateStart: new FormControl('', [Validators.required]),
-      dateEnd: new FormControl('', [Validators.required]),
+      type: new FormControl(''),
+      channel: new FormControl(''),
+      currency: new FormControl(''),
+      senderNumber: new FormControl(''),
+      recieverNumber: new FormControl(''),
+      transactionNumber: new FormControl(''),
     });
   }
 }
