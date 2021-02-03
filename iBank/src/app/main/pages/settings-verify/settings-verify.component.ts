64621fa69d3f88c74ec51e630242c2f1d5ab612e
bilguungt <bilguungt2@gmail.com>
2021-02-03 18:42:48 +0800

    settings verify bolon add location

--- iBank/src/app/main/pages/settings-verify/settings-verify.component.ts
+++ iBank/src/app/main/pages/settings-verify/settings-verify.component.ts
@@ -5,6 +5,7 @@ import { MgCmerchGetMerchVerifyReq, MgCmerchGetMerchVerifyRes } from 'src/app/co
 import { DialogComponent } from '../../fragments/dialog/dialog.component';
 import { ApiService } from 'src/app/core/services/api.service';
 import { MgCmerchGetMerchVerfReq } from 'src/app/core/model/payment/MgCmerchGetMerchVerfReq';
+import { IgxTimePickerComponent } from 'igniteui-angular';
 
 
 @Component({
@@ -17,18 +18,19 @@ export class SettingsVerifyComponent implements OnInit {
   verifstatus: MgCmerchGetMerchVerifyRes;
   verifType: string;
   savedLocData:any;
+  reqTest: MgCmerchGetMerchVerifyRes;
 
 
   constructor(public dialog: MatDialog, private api: ApiService,) { }
 
   ngOnInit(): void {
     this.merchCode = sessionStorage.getItem('merchant');
-    this.verify();
+    this.checkStatus();
     this.savedLocData = null;
-
+    console.log(this.savedLocData);
   }
 
-  verify() {
+  checkStatus() {
 
     const req = new MgCmerchGetMerchVerifyReq(this.merchCode);
     console.log(req);
@@ -52,7 +54,43 @@ export class SettingsVerifyComponent implements OnInit {
      this.savedLocData = result as MgCmerchGetMerchVerfReq;
       console.log(this.savedLocData);
     });
+  }
 
+verify() {
+    // // ene functiong add location bish verify deer ashiglana.
+    if(this.savedLocData!=null) {
+      this.reqTest = this.savedLocData;
+      console.log(this.reqTest);
 
+      this.api.verifyMerch(this.savedLocData).subscribe(data =>
+        {
+
+       console.log(data);
+      //   console.log(data1.responseDesc);
+      });
+    }
+    console.log("a00");
+    
+  }
+
+  
+  public today: Date = new Date();
+
+  public onBlur(inputValue: string, value: Date, picker: IgxTimePickerComponent) {
+      const parts = inputValue.split(/[\s:]+/);
+
+      const hour = parseInt(parts[0], 10);
+      const minutes = parseInt(parts[0], 10);
+
+      if (picker.validHourEntries.indexOf(hour) !== -1 && picker.validMinuteEntries.indexOf(minutes) !== -1) {
+          value.setHours(hour, minutes);
+      } else {
+          throw new Error("This is not a valid hour.");
+      }
+  }
+
+  public selectNow(timePicker: IgxTimePickerComponent) {
+      timePicker.value = this.today;
+      timePicker.close();
   }
 }
