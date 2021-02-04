35162b5d6ded46ac499a0c52430f85ac3b2c2a3f
bilguungt <bilguungt2@gmail.com>
2021-02-04 19:26:27 +0800

    settings general

--- iBank/src/app/core/model/Online user registration/regMerch.ts
+++ iBank/src/app/core/model/Online user registration/regMerch.ts
@@ -12,29 +12,29 @@ export class MgCmerchRegMerchReq {
   isVatPayer: number;
 
   constructor(
-    // merchCode: string,
-    // icon: string,
-    // name: string,
-    // orgTypeId: number,
-    // registerCode: string,
-    // address: string,
-    // msisdn: string,
-    // phone: string,
-    // email: string,
-    // notifType: string,
-    // isVatPayer: number
+    merchCode: string,
+    icon: string,
+    name: string,
+    orgTypeId: number,
+    registerCode: string,
+    address: string,
+    msisdn: string,
+    phone: string,
+    email: string,
+    notifType: string,
+    isVatPayer: number
   ) {
-    // this.merchCode = merchCode
-    // this.icon = icon
-    // this.name = name
-    // this.orgTypeId = orgTypeId
-    // this.registerCode = registerCode
-    // this.address = address
-    // this.msisdn = msisdn
-    // this.phone = phone
-    // this.email = email
-    // this.notifType = notifType
-    // this.isVatPayer = isVatPayer
+    this.merchCode = merchCode
+    this.icon = icon
+    this.name = name
+    this.orgTypeId = orgTypeId
+    this.registerCode = registerCode
+    this.address = address
+    this.msisdn = msisdn
+    this.phone = phone
+    this.email = email
+    this.notifType = notifType
+    this.isVatPayer = isVatPayer
   }
 
 }
--- iBank/src/app/main/pages/settings-general/settings-general.component.ts
+++ iBank/src/app/main/pages/settings-general/settings-general.component.ts
@@ -16,6 +16,8 @@ import {
 import { ApiService } from 'src/app/core/services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MgCmerchGetMerchVerifyReq } from 'src/app/core/model/payment/getMerchVerify';
+import { MgCmerchRegMerchReq } from 'src/app/core/model/Online user registration/regMerch';
+import { NumberFormatStyle } from '@angular/common';
 @Component({
   selector: 'app-settings-general',
   templateUrl: './settings-general.component.html',
@@ -27,7 +29,7 @@ export class SettingsGeneralComponent implements OnInit {
     private api: ApiService,
     private router: Router,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   accountDet: MgCmerchGetMerchRegRes;
   dicOrg: MgLoginDicData;
@@ -37,21 +39,22 @@ export class SettingsGeneralComponent implements OnInit {
   merchCode: string;
   compSett: FormGroup;
   toggler: boolean;
+  togglerValue: Number;
   sub: any;
   tabIndex: Number;
 
   ngOnInit(): void {
     this.merchCode = sessionStorage.getItem('merchant');
+
     this.compSett = this.formBuilder.group({
       merchName: new FormControl('', [Validators.required]),
       registerCode: new FormControl('', [Validators.required]),
-      adress: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
       phone: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required]),
-      isChecked: new FormControl(this.toggler, [Validators.required]),
-      orgTypeId: new FormControl(this.accountDet?.orgTypeId, [
-        Validators.required,
-      ]),
+      isChecked: new FormControl(false, [Validators.required]),
+      orgTypeId: new FormControl(''),
+      notifChannel: new FormControl(''),
     });
 
     this.sub = this.route.snapshot.paramMap.get('tabStatus');
@@ -59,10 +62,13 @@ export class SettingsGeneralComponent implements OnInit {
       this.tabIndex = 1;
     }
 
+    this.getDictionary();
+
     this.Init();
+
   }
 
-  setTabIndex(index){
+  setTabIndex(index) {
     console.log(index);
     this.tabIndex = index;
   }
@@ -71,70 +77,86 @@ export class SettingsGeneralComponent implements OnInit {
     this.api.getDictionary('dicPayeeOrgType').subscribe((data) => {
       if (data.responseCode == 0) {
         this.dicOrg = data.responseData;
-        // this.compSett.get('orgTypeId').setValue(this.accountDet?.orgTypeId);
-        console.log(this.dicOrg);
-        // var selected = this.dicOrg.id   ;
-        // console.log(selected);
       }
-      console.log(data);
+      console.log(this.dicOrg);
     });
     this.api.getDictionary('dicPayeeNotifType').subscribe((data1) => {
       if (data1.responseCode == 0) {
         this.dicNotif = data1.responseData;
-        console.log(this.dicNotif);
       }
     });
   }
-  GetUserInfo() {
+
+
+
+  GetCompanyInfo() {
     const req = new MgCmerchGetMerchRegReq(this.merchCode);
-    this.api.getMerchReg(req).subscribe((data) => {
+    this.api.getMerchReg(req).subscribe(data => {
       if (data.responseCode != 0) {
       }
       if (data.responseCode == 0) {
         this.accountDet = data;
-        console.log(this.accountDet);
         this.verfStat = this.accountDet.verfStatus;
-        console.log(this.verfStat);
         if (this.accountDet.isVatPayer == 1) {
           this.toggler = true;
+          this.compSett.value.isChecked = true;
         } else {
           this.toggler = false;
         }
-        console.log(this.toggler);
+        
       }
+      console.log(this.accountDet);
+      this.setFirstValue();
     });
-    this.getDictionary();
+  }
+
+  setFirstValue() {
+    // this.compSett.value.merchName = this.accountDet?.merchName;
+
+    this.compSett.controls['orgTypeId'].setValue(this.accountDet?.orgTypeId.toString());
+    this.compSett.controls['notifChannel'].setValue(this.accountDet?.notifType);
+    this.compSett.controls['merchName'].setValue(this.accountDet?.merchName);
+    this.compSett.controls['phone'].setValue(this.accountDet?.phone);
+    this.compSett.controls['email'].setValue(this.accountDet?.email);
+    
+    this.compSett.controls['address'].setValue(this.accountDet?.address);
+    this.compSett.controls['registerCode'].setValue(this.accountDet?.registerCode);
+
+    console.log(this.compSett.value.orgTypeId);
+    console.log(this.compSett.value.merchName);
+
+
+  }
+  saveCompanyInfo() {
+    this.compSett.value.merchName = this.accountDet?.merchName;
+    
+    // this.compSett.value.registerCode = this.accountDet?.registerCode;
+    // this.compSett.value.address = this.accountDet?.address;
+    this.compSett.value.msisdn = this.accountDet?.phone;
+    // this.compSett.value.phone = this.accountDet?.phone;
+    // this.compSett.value.email = this.accountDet?.email;
+    // this.compSett.controls['orgTypeId'].setValue(this.accountDet?.orgTypeId.toString());
+    // this.compSett.controls['notifChannel'].setValue(this.accountDet?.notifType);
+    const companyInfo = new MgCmerchRegMerchReq(this.merchCode, "icon", this.compSett.value.merchName, this.compSett.value.orgTypeId, this.compSett.value.registerCode, this.compSett.value.address, this.compSett.value.phone, this.compSett.value.phone, this.compSett.value.email, this.compSett.value.notifChannel, 1);
+    // ene test hiih companyInfo
+    // const companyInfo = new MgCmerchRegMerchReq(this.merchCode,"icon",this.compSett.value.merchName,this.compSett.value.orgTypeId, '123123', 'bzd 13 horoo 119a', '94192219', '99192219' , 'bilguungt2@gmail.com', this.compSett.value.notifChannel ,1 );
+    console.log(companyInfo);
+    this.api.regMerch(companyInfo).subscribe(data => {
+      console.log(data);
+    }
+    )
   }
 
   doSomething(value) {
     console.log(this.compSett.value.orgTypeId);
+    console.log(this.compSett.value.notifChannel);
+    console.log(this.compSett.value.isChecked);
   }
   verify() {
     this.router.navigate(['/settings/verify']);
   }
 
   Init() {
-    this.GetUserInfo();
-
-    let info = new MgCmerchGetMerchRegRes(
-      'merchCode',
-      'merchName',
-      'custCode',
-      'custName',
-      0,
-      0,
-      'orgTypeName',
-      'registerCode',
-      'address',
-      0,
-      'msisdn',
-      'phone',
-      'email',
-      'notifType',
-      'verfStatus',
-      'verfStatusName',
-      0,
-      'responseDesc'
-    );
+    this.GetCompanyInfo();
   }
 }
